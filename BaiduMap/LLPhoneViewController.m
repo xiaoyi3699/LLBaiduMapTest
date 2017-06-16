ec1454ff423628dd3d4c7acab58c9613276af4ef
王 <122589615@qq.com>
2017-06-16 15:22:15 +0800

    优化

--- BaiduMap/LLPhoneViewController.m
+++ BaiduMap/LLPhoneViewController.m
@@ -9,9 +9,12 @@
 #import "LLPhoneViewController.h"
 
 @interface LLPhoneViewController (){
-    NSTimer    *_timer;
-    UIButton   *_codeBtn;
-    NSInteger  _remainingTime;
+    NSTimer     *_timer;
+    UIButton    *_codeBtn;
+    UIButton    *_OKBtn;
+    NSInteger   _remainingTime;
+    UITextField *_phoneTextField;
+    UITextField *_codeTextField;
 }
 
 @end
@@ -26,8 +29,8 @@
     return self;
 }
 
-- (void)viewWillDisappear:(BOOL)animated {
-    [super viewWillDisappear:animated];
+- (void)viewDidDisappear:(BOOL)animated {
+    [super viewDidDisappear:animated];
     if (_timer) {
         [_timer invalidate];
         _timer = nil;
@@ -55,19 +58,21 @@
     inputView.backgroundColor = [UIColor whiteColor];
     [self.view addSubview:inputView];
     
-    UITextField *phoneTextField = [[UITextField alloc] initWithFrame:CGRectMake(10, 10, CGRectGetWidth(inputView.frame)-100, 30)];
-    phoneTextField.placeholder = @"手机号";
-    phoneTextField.keyboardType = UIKeyboardTypeNumberPad;
-    [inputView addSubview:phoneTextField];
+    _phoneTextField = [[UITextField alloc] initWithFrame:CGRectMake(10, 10, CGRectGetWidth(inputView.frame)-100, 30)];
+    _phoneTextField.placeholder = @"手机号";
+    _phoneTextField.keyboardType = UIKeyboardTypeNumberPad;
+    [_phoneTextField addTarget:self action:@selector(textFieldValueChanged:) forControlEvents:UIControlEventEditingChanged];
+    [inputView addSubview:_phoneTextField];
     
-    UIView *lineView = [[UIView alloc] initWithFrame:CGRectMake(10, CGRectGetMaxY(phoneTextField.frame)+10, SCREEN_WIDTH-20, 1)];
+    UIView *lineView = [[UIView alloc] initWithFrame:CGRectMake(10, CGRectGetMaxY(_phoneTextField.frame)+10, SCREEN_WIDTH-20, 1)];
     lineView.backgroundColor = [UIColor colorWithRed:222/255. green:222/255. blue:222/255. alpha:1];
     [inputView addSubview:lineView];
     
-    UITextField *codeTextField = [[UITextField alloc] initWithFrame:CGRectMake(10, CGRectGetMaxY(lineView.frame)+10, CGRectGetWidth(inputView.frame)-20, 30)];
-    codeTextField.placeholder = @"验证码";
-    codeTextField.keyboardType = UIKeyboardTypeASCIICapable;
-    [inputView addSubview:codeTextField];
+    _codeTextField = [[UITextField alloc] initWithFrame:CGRectMake(10, CGRectGetMaxY(lineView.frame)+10, CGRectGetWidth(inputView.frame)-20, 30)];
+    _codeTextField.placeholder = @"验证码";
+    _codeTextField.keyboardType = UIKeyboardTypeNumberPad;
+    [_codeTextField addTarget:self action:@selector(textFieldValueChanged:) forControlEvents:UIControlEventEditingChanged];
+    [inputView addSubview:_codeTextField];
     
     _codeBtn = [UIButton buttonWithType:UIButtonTypeCustom];
     _codeBtn.tag = 0;
@@ -78,25 +83,27 @@
     [_codeBtn setTitle:@"发送验证码" forState:UIControlStateNormal];
     [_codeBtn setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
     [_codeBtn setBackgroundImage:[UIImage ll_imageWithColor:[UIColor colorWithRed:60/255. green:170/255. blue:70/255. alpha:1]] forState:UIControlStateNormal];
+    _codeBtn.enabled = NO;
     [_codeBtn addTarget:self action:@selector(btnClick:) forControlEvents:UIControlEventTouchUpInside];
     [inputView addSubview:_codeBtn];
     
-    UIButton *OKBtn = [UIButton buttonWithType:UIButtonTypeCustom];
-    OKBtn.tag = 1;
-    OKBtn.frame = CGRectMake(10, CGRectGetMaxY(inputView.frame)+20, SCREEN_WIDTH-20, 40);
-    OKBtn.layer.masksToBounds = YES;
-    OKBtn.layer.cornerRadius = 5;
-    OKBtn.enabled = NO;
-    [OKBtn setTitle:@"确定" forState:UIControlStateNormal];
-    [OKBtn setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
-    [OKBtn setBackgroundImage:[UIImage ll_imageWithColor:[UIColor redColor]] forState:UIControlStateNormal];
-    [OKBtn addTarget:self action:@selector(btnClick:) forControlEvents:UIControlEventTouchUpInside];
-    [self.view addSubview:OKBtn];
+    _OKBtn = [UIButton buttonWithType:UIButtonTypeCustom];
+    _OKBtn.tag = 1;
+    _OKBtn.frame = CGRectMake(10, CGRectGetMaxY(inputView.frame)+20, SCREEN_WIDTH-20, 40);
+    _OKBtn.layer.masksToBounds = YES;
+    _OKBtn.layer.cornerRadius = 5;
+    _OKBtn.enabled = NO;
+    [_OKBtn setTitle:@"确定" forState:UIControlStateNormal];
+    [_OKBtn setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
+    [_OKBtn setBackgroundImage:[UIImage ll_imageWithColor:[UIColor redColor]] forState:UIControlStateNormal];
+    [_OKBtn addTarget:self action:@selector(btnClick:) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:_OKBtn];
 }
 
 - (void)btnClick:(UIButton *)btn {
     if (btn.tag == 0) {//发送验证码
         btn.enabled = NO;
+        _phoneTextField.enabled = NO;
         _remainingTime = 59;
         if (_timer == nil) {
             _timer = [NSTimer scheduledTimerWithTimeInterval:1.0 target:self selector:@selector(timerRun) userInfo:nil repeats:YES];
@@ -107,6 +114,31 @@
     }
 }
 
+- (void)textFieldValueChanged:(UITextField *)textField {
+    
+    if (_phoneTextField.text.length >= 11 && _codeTextField.text.length >= 6) {
+        _OKBtn.enabled = YES;
+    }
+    else {
+        _OKBtn.enabled = NO;
+    }
+    
+    if (textField ==_phoneTextField) {//手机号
+        if (textField.text.length >= 11) {
+            textField.text = [textField.text substringToIndex:11];
+            _codeBtn.enabled = YES;
+        }
+        else {
+            _codeBtn.enabled = NO;
+        }
+    }
+    else {//验证码
+        if (textField.text.length >= 6) {
+            textField.text = [textField.text substringToIndex:6];
+        }
+    }
+}
+
 - (void)timerRun {
     if (_remainingTime > 0) {
         NSString *remainingTimeStr = [NSString stringWithFormat:@"%lds",(long)_remainingTime];
@@ -114,6 +146,7 @@
     }
     else {
         _codeBtn.enabled = YES;
+        _phoneTextField.enabled = YES;
         [_codeBtn setTitle:@"发送验证码" forState:UIControlStateNormal];
         if (_timer) {
             [_timer invalidate];
